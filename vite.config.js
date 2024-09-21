6b5e350ca3382786605bf254a8d763419d9591bc
mraihanhatim <raihan04042001@gmail.com>
2024-09-21 13:01:09 +0700

    delete

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reactJs/",
   test: {
     globals: true,
     environment: "jsdom",
