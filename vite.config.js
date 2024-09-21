94f0e132c3da681d6189b7365d433e64399a73a4
mraihanhatim <raihan04042001@gmail.com>
2024-09-21 12:50:25 +0700

    edit: workflows

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/reactJs/",
   test: {
     globals: true,
     environment: "jsdom",
